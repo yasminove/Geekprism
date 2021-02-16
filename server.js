94f81b5a2f067c07c778be977b654e5ca4dbc415
Yasmin Hillis <jasminehillis94@gmail.com>
2021-02-16 15:56:23 +0200

    routes setup

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ connectDB();
 
 app.get('/', (req, res) => res.send('api running'));
 
+app.use('/api/users', require('./routes/api/users'));
+
+app.use('/api/profile', require('./routes/api/profile'));
+
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
